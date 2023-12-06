d940631b5bfacecba19aad3d27722cd652d73424
walterwhiteita <dimieri00@gmail.com>
2023-12-06 17:08:26 +0100

    Unary Canonic implementation

--- ScientificCalculator/src/main/java/scientificcalculator/classes/UnaryCanonicOperations.java
+++ ScientificCalculator/src/main/java/scientificcalculator/classes/UnaryCanonicOperations.java
@@ -1,15 +1,31 @@
 package scientificcalculator.classes;
 
+import static java.lang.Math.PI;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+
 /**
  *
  * @author angel
  */
 public final class UnaryCanonicOperations {
     public static ComplexNumber[] squareRoot(ComplexNumber n){
-        ComplexNumber [] n1 = new ComplexNumber[2];
-        return n1;
+        ComplexNumber[] radix = new ComplexNumber[2];
+        //Calcolo la prima radice
+        double realPart = sqrt(n.getModule())*cos(n.getPhase()/2);
+        double imaginaryPart = sqrt(n.getModule())*sin(n.getPhase()/2);
+        //Inserisco il numero all'interno del vettore
+        radix[0]=new ComplexNumber(realPart,imaginaryPart);
+        //Calcolo la seconda radice
+        realPart = sqrt(n.getModule())*cos((n.getPhase()+2*PI)/2);
+        imaginaryPart = sqrt(n.getModule())*sin((n.getPhase()+2*PI)/2);
+        //Inserisco il numero all'interno del vettore
+        radix[1]=new ComplexNumber(realPart,imaginaryPart);
+        //Ritorno il vettore
+        return radix;
     }
     public static ComplexNumber changeSign(ComplexNumber n){
-        return n;
+        return new ComplexNumber((n.getRealPart())*(-1),(n.getImaginaryPart())*(-1));
     }
 }
