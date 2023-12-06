1952261f76fb6e94e5f089f55c655bf216fa3696
Gianmarco Uni <Gianmarco Uni@LAPTOP-O37IQ9O7>
2023-12-06 16:03:36 +0100

    implementazione BinaryCanonicOperations

--- ScientificCalculator/src/main/java/scientificcalculator/classes/BinaryCanonicOperations.java
+++ ScientificCalculator/src/main/java/scientificcalculator/classes/BinaryCanonicOperations.java
@@ -1,17 +1,43 @@
 package scientificcalculator.classes;
+import static java.lang.Math.*;
 
 public final class BinaryCanonicOperations {
     public static ComplexNumber sum(ComplexNumber n1, ComplexNumber n2){
-        return n1;
+        
+        double realPartResult = n1.getRealPart() + n2.getRealPart();
+        double imaginaryPartResult = n1.getImaginaryPart() + n2.getImaginaryPart();
+        
+        ComplexNumber result = new ComplexNumber(realPartResult, imaginaryPartResult);
+        return result;
         
     }
     public static ComplexNumber sub(ComplexNumber n1, ComplexNumber n2){
-        return n1;
+        double realPartResult = n1.getRealPart() - n2.getRealPart();
+        double imaginaryPartResult = n1.getImaginaryPart() - n2.getImaginaryPart();
+        
+        ComplexNumber result = new ComplexNumber(realPartResult, imaginaryPartResult);
+        return result;
     }
     public static ComplexNumber multiply(ComplexNumber n1, ComplexNumber n2){
-        return n1;
+        
+        double resultModule = n1.getModule() * n2.getModule();
+        double resultPhase = n1.getPhase() + n2.getPhase();
+        double resultRealPart = resultModule * cos(resultPhase);
+        double resultImaginaryPart = resultModule * sin(resultPhase);
+        
+        
+        ComplexNumber result = new ComplexNumber(resultRealPart, resultImaginaryPart);
+        return result;
     }
     public static ComplexNumber divide(ComplexNumber n1, ComplexNumber n2){
-        return n1;
+        
+        double resultModule = n1.getModule() / n2.getModule();
+        double resultPhase = n1.getPhase() - n2.getPhase();
+        double resultRealPart = resultModule * cos(resultPhase);
+        double resultImaginaryPart = resultModule * sin(resultPhase);
+        
+        
+        ComplexNumber result = new ComplexNumber(resultRealPart, resultImaginaryPart);
+        return result;
     }
 }
