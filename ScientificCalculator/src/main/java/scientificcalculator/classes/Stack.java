9df1c3e1b641335892faaaf51ff4933da4e4ad8b
Gianmarco Uni <Gianmarco Uni@LAPTOP-O37IQ9O7>
2023-12-06 17:48:41 +0100

    implementazione Stack

--- ScientificCalculator/src/main/java/scientificcalculator/classes/Stack.java
+++ ScientificCalculator/src/main/java/scientificcalculator/classes/Stack.java
@@ -1,40 +1,76 @@
 package scientificcalculator.classes;
 
 import java.util.Deque;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 public class Stack {
+
     private Deque<ComplexNumber> stack;
     //Only for debug
     private ComplexNumber n;
-    
+
     public Stack() {
+        stack = new LinkedList();
     }
-    public void push(ComplexNumber input){
-        
+
+    public void push(ComplexNumber input) {
+        stack.addFirst(input);
     }
-    public ComplexNumber pop(){
-        return n;
+
+    public ComplexNumber pop() {
+        ComplexNumber c = null;
+        try{
+            c = stack.peekFirst();stack.removeFirst();
+        }catch(NoSuchElementException ex){
+            System.out.println("Stack vuoto gay!");
+        }
+        return c;
     }
-    public ComplexNumber top(){
-        return n;
+
+    public ComplexNumber top() {
+        ComplexNumber c = null;
+        try{
+            c = stack.peekFirst();
+        }catch(NoSuchElementException ex){
+            System.out.println("Stack vuoto gay!");
+        }
+        return c;
     }
-    public void clear(){
-        
+
+    public void clear() {
+        stack.clear();
+
     }
-    public void drop(){
-        
+
+    public void drop() {
+        this.pop();
     }
-    public void swap(){
-        
+
+    public void swap() {
+        ComplexNumber last = this.pop();
+        ComplexNumber beforeLast = this.pop();
+        this.push(last);
+        this.push(beforeLast);
+
     }
-    public void duplicate(){
-        
+
+    public void duplicate() {
+        ComplexNumber last = this.top();
+        stack.push(last);
+
     }
-    public void over(){
-        
+
+    public void over() {
+        ComplexNumber last = this.pop();
+        ComplexNumber beforeLast = this.top();
+        this.push(last);
+        this.push(beforeLast);
+
     }
+
     public Deque<ComplexNumber> getStack() {
         return stack;
     }
-    
+
 }
